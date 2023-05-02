60e24b4f51d095741e37762794f476a7aa4c2e44
DR <94643493+DanilRodenko@users.noreply.github.com>
2023-05-02 18:46:58 +0100

    Add links, specify language, limit news, subject and control category

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import requests
 from send_mail import send_mail
 
 
-
+topic = "tesla"
 api_key = "ee82603e140342af8eb68c4119307ab0"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2023-04-02&sortBy=publishedAt&api" \
-      "Key=ee82603e140342af8eb68c4119307ab0"
+url = "https://newsapi.org/v2/everything" \
+      f"?q={topic}" \
+      "&from=2023-04-02&" \
+      "sortBy=publishedAt&" \
+      "apiKey=ee82603e140342af8eb68c4119307ab0&" \
+      "language=en"
 
 request = requests.get(url)
 content = request.json()
 message = ''
-for article in content['articles']:
-    message = message + article['title'] + "\n" + article['description'] + 2*'\n'
+for article in content['articles'][:20]:
+    if article["title"] is not None:
+        message = "Subject: Today News" \
+                  + "\n" + message + article['title']\
+                  + "\n" + article['description']\
+                  +'\n' + article['url'] + 2*'\n'
 
 send_mail(message=message)
 
